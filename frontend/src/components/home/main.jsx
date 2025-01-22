65e4533c5f173214711acd7f5df6d1ce1e4c440a
AdipRajak <np05cp4s230070@iic.edu.np>
2025-01-22 19:30:56 +0545

    Y

--- frontend/src/components/home/main.jsx
+++ frontend/src/components/home/main.jsx
@@ -7,21 +7,21 @@ const Main = () => {
         <div className="flex justify-between items-center flex-col md:flex-row gap-8 h-[90vh]">
             {/* Left Side: Image */}
             <div className="flex-1">
-                <img src={Mysvg} alt="Vet System" className="w-full h-auto" />
+                <img src={Mysvg} alt="Agrohealth & Service" className="w-full h-auto" />
             </div>
 
             {/* Right Side: Text and Button */}
             <div className="flex-1 flex flex-col gap-4 justify-center items-center">
                 <div className="space-y-3 flex flex-col">
                     <h1 className="font-bold text-[35px] sm:text-[45px] md:text-[55px] text-primary">
-                        Growing Dreams, <br />Harvesting Success
+                    Connecting Farmers, Vets, and Progress, <br />for a Healthier Tomorrow!
                     </h1>
                     <p className="font-medium text-justify text-sm sm:text-base md:text-lg">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Vero veritatis possimus soluta sit cupiditate,
-                        nemo, modi corrupti quaerat eius dolorum reiciendis
-                        quasi magnam obcaecati eaque? Iste nobis aliquam eveniet
-                        vero.
+                    AgroHealth & Service is an innovative online platform designed
+                    to revolutionize cattle farming by seamlessly connecting farmers, 
+                    veterinary doctors, and local government authorities.
+                    Our platform empowers farmers to access timely veterinary care, apply for subsidies, 
+                    and stay informed about the latest in cattle farming practices.
                     </p>
                     <div className="flex justify-center md:justify-end">
                         <button className="flex gap-2 items-center px-9 bg-primary border-2 border-primary text-white py-3 rounded-sm font-semibold transition-all duration-500 ease-in-out hover:bg-white hover:text-primary">
