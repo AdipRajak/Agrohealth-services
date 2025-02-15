f5dc45578895b854077748f9beb6ccf3f45a5836
AdipRajak <np05cp4s230070@iic.edu.np>
2025-02-15 21:44:47 +0545

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,15 @@ import "./app.css";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import Topbar from "./components/admin/TopBar";
 import AdminMain from "./pages/AdminMain";
-import User from "./components/admin/User";
+import User from "./components/admin/user/User";
+import Dashboard from "./components/admin/global/Dashboard";
+import Doctor from "./components/admin/doctor/Doctor";
+import Post from "./components/admin/post/Post";
+import UserAdd from "./components/admin/user/UserAdd";
+import DoctorAdd from "./components/admin/doctor/DoctorAdd";
+import UserUpdate from "./components/admin/user/UserUpdate";
+import PostAdd from "./components/admin/post/PostAdd";
 
 const App = () => {
     // User Layout with Navbar
@@ -52,8 +58,36 @@ const App = () => {
             element: <AdminLayout />,
             children: [
                 {
+                    path: "",
+                    element: <Dashboard />,
+                },
+                {
                     path: "user",
                     element: <User />,
+                },
+                {
+                    path: "adduser",
+                    element: <UserAdd />,
+                },
+                {
+                    path: "updateuser/:id",
+                    element: <UserUpdate />,
+                },
+                {
+                    path: "doctor",
+                    element: <Doctor />,
+                },
+                {
+                    path: "adddoctor",
+                    element: <DoctorAdd />,
+                },
+                {
+                    path: "post",
+                    element: <Post />,
+                },
+                {
+                    path: "addpost",
+                    element: <PostAdd />,
                 },
             ],
         },
@@ -61,7 +95,7 @@ const App = () => {
 
     return (
         <div className="flex justify-center min-h-screen">
-            <div className="App font-heading w-full max-w-[343px]  md:max-w-[704px] lg:max-w-[1240px] xl:max-w-[100%]">
+            <div className="App font-heading w-full max-w-[343px]  md:max-w-[704px] lg:max-w-[1240px] ">
                 <RouterProvider router={router} />
             </div>
         </div>
---
+++ frontend/src/components/admin/user/User.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { get } from "../../../utils/api";
+
+const User = () => {
+    const navigate = useNavigate();
+    const [data, setData] = useState([]);
+    console.log(data);
+
+    const fetchData = async () => {
+        const res = await get('/api/get-user', {});
+        setData(res);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const handleUpdate = (id) => {
+        navigate(`/admin/updateuser/${id}`, {
+            state: { user_id: id }, // Passing the row details as state
+        });
+    };
+
+    return (
+        <div className="flex-1 shadow-lg shadow-gray-300 rounded-md px-3 py-3">
+            <div className="flex justify-between">
+                <h1 className="font-medium text-[30px] text-primary">
+                    All User
+                </h1>
+                <button className="bg-[#437EF7] py-2 px-6 rounded-md text-white">
+                    <Link to={`/admin/adduser`}>
+                        Add User
+                    </Link>
+                </button>
+            </div>
+            <div className="overflow-x-auto my-6">
+                <table className="min-w-full table-auto border-separate border-spacing-0">
+                    <thead>
+                        <tr className="bg-gray-100">
+                            <th className="px-4 py-2 border-b text-left text-sm font-medium text-gray-700">
+                                ID
+                            </th>
+                            <th className="px-4 py-2 border-b text-left text-sm font-medium text-gray-700">
+                                Name
+                            </th>
+                            <th className="px-4 py-2 border-b text-left text-sm font-medium text-gray-700">
+                                Address
+                            </th>
+                            <th className="px-4 py-2 border-b text-left text-sm font-medium text-gray-700">
+                                Email
+                            </th>
+                            <th className="px-4 py-2 border-b text-left text-sm font-medium text-gray-700">
+                                Phone
+                            </th>
+                            <th className="px-4 py-2 border-b text-left text-sm font-medium text-gray-700">
+                                Action
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((row) => (
+                            <tr key={row.id} className="hover:bg-gray-50">
+                                <td className="px-4 py-2 border-b text-sm text-gray-800">
+                                    {row.id}
+                                </td>
+                                <td className="px-4 py-2 border-b text-sm text-gray-800">
+                                    {row.name}
+                                </td>
+                                <td className="px-4 py-2 border-b text-sm text-gray-800">
+                                    {row.address}
+                                </td>
+                                <td className="px-4 py-2 border-b text-sm text-gray-800">
+                                    {row.email}
+                                </td>
+                                <td className="px-4 py-2 border-b text-sm text-gray-800">
+                                    {row.phone}
+                                </td>
+                                <td className="px-4 py-2 border-b text-sm text-gray-800 flex gap-4">
+                                    <button
+                                        className="bg-[#437EF7] text-white px-6 py-2 rounded-md"
+                                        onClick={() => handleUpdate(row.id)} // Pass the row object
+                                    >
+                                        Update
+                                    </button>
+                                    <button className="bg-red-500 text-white px-6 py-2 rounded-md">
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default User;
